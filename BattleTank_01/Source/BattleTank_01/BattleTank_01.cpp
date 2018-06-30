700b6232bd9ee442ef1b5e7df508cd4b2373dde3
DESKTOP-DH54RRN\svanh <Svanhoot@yahoo.com>
2018-06-30 13:54:37 -0400

    Lecture 107 end, adding a landscape

---
+++ BattleTank_01/Source/BattleTank_01/BattleTank_01.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank_01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleTank_01, "BattleTank_01" );
---
+++ BattleTank_01/Source/BattleTank_01/BattleTank_01GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTank_01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_01_API ABattleTank_01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
