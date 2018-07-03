3e7f9d1fc91a571b1fee41f7460ab3de5e688b8f
DESKTOP-DH54RRN\svanh <Svanhoot@yahoo.com>
2018-07-03 17:42:23 -0400

    L133

--- BattleTank_01/Source/BattleTank_01/Private/TankPlayerController.cpp
+++ BattleTank_01/Source/BattleTank_01/Private/TankPlayerController.cpp
@@ -18,9 +18,34 @@ void ATankPlayerController::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
+
 ATank * ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank())
+	{
+		return;
+	}
+	FVector HitLocation;
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *HitLocation.ToString())
+	}
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+	OutHitLocation = FVector(1.0f);
+	return true;
 }
 
 
