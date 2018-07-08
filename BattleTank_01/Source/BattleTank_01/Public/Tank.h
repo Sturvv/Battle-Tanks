755434e106cc6e90b1732ede62004842c3972012
DESKTOP-DH54RRN\svanh <Svanhoot@yahoo.com>
2018-07-08 14:47:34 -0400

    Lecture 139

--- BattleTank_01/Source/BattleTank_01/Public/Tank.h
+++ BattleTank_01/Source/BattleTank_01/Public/Tank.h
@@ -18,21 +18,23 @@ public:
 	// Sets default values for this pawn's properties
 	ATank();
 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void AimAt(FVector HitLocation);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	UTankAimingComponent* TankAimingComponent = nullptr;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:	
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
-	void AimAt(FVector HitLocation);
-
-	
-	
 };
