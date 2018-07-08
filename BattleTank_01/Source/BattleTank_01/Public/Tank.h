065981d8d321096d079a048d9c1ab1308bab0701
DESKTOP-DH54RRN\svanh <Svanhoot@yahoo.com>
2018-07-08 19:14:20 -0400

    Lecture 144 end

--- BattleTank_01/Source/BattleTank_01/Public/Tank.h
+++ BattleTank_01/Source/BattleTank_01/Public/Tank.h
@@ -7,7 +7,7 @@
 #include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
-
+class UTankBarrel;
 
 UCLASS()
 class BATTLETANK_01_API ATank : public APawn
@@ -27,7 +27,7 @@ public:
 	void AimAt(FVector HitLocation);
 
 	UFUNCTION(BlueprintCallable, Category = Setup)
-	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+	void SetBarrelReference(UTankBarrel* BarrelToSet);
 
 protected:
 	// Called when the game starts or when spawned
